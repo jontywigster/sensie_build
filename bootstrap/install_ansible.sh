a9725a309be8a4c8c26b502f3c4cd1709846f28a
Wigster <53944826+jontywigster@users.noreply.github.com>
2024-10-30 21:57:15 +0100

    avoid proliferation of playbooks in root

--- bootstrap/install_ansible.sh
+++ bootstrap/install_ansible.sh
@@ -5,7 +5,6 @@ source /opt/sensie_build/bootstrap/set_pipx_env.sh
 #pipx install ansible-core
 #as at 14/10/24 ansible-core w/pipx had
 #trouble using galaxy collection. 
-#Script wouldn't run - couldn't resolve module before actually installing mod
 pipx install --include-deps ansible
 
 chmod +x /opt/sensie_build/*.sh
