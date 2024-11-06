6bf559148e3858564861e5c8f7f99a85f5dad5e4
Wigster <53944826+jontywigster@users.noreply.github.com>
2024-11-06 19:24:45 +0100

    kvp flag install ansible

--- bootstrap/install_ansible.sh
+++ bootstrap/install_ansible.sh
@@ -2,6 +2,8 @@
 
 source /opt/sensie_build/bootstrap/set_pipx_env.sh
 
+printf "sensie_build%0500dinan%02044d" | tr '0' '\0' > /var/lib/hyperv/.kvp_pool_1
+
 #pipx install ansible-core
 #as at 14/10/24 ansible-core w/pipx had
 #trouble using galaxy collection. 
